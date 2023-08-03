89d21c98c4d4dc985894da13e58b817251c2b721
jayricka <jayricka17@gmail.com>
2023-08-04 02:13:24 +0300

    bugs out

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -19,6 +19,6 @@ return (0);
 left_height = binary_tree_height(tree->left);
 right_height = binary_tree_height(tree->right);
 
-return ((left_height > right_height ? left_height : right_height) + 1);
+return ((left_height > right_height ? left_height : right_height)+1);
 }
 
